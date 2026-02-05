350c3be3bd139448de357ac2f0e4f7574b182ff6
DESKTOP-F2A2EJL\Playdata <tjsdn8963@naver.com>
2026-02-05 15:18:31 +0900

    feat: 기본 타입 입출력 보조 스트림 실습 정리 추가

--- 22_AssistStream/src/com/beyond/assiststream/practice/C_DataStream.java
+++ 22_AssistStream/src/com/beyond/assiststream/practice/C_DataStream.java
@@ -1,7 +1,33 @@
 package com.beyond.assiststream.practice;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class C_DataStream {
     public void fileSaveAndRead() {
-        
+        try (DataOutputStream dos = new DataOutputStream( new FileOutputStream("b_data.txt"));
+             DataInputStream dis = new DataInputStream(new FileInputStream("b_data.txt"))) {
+
+            // 파일에 데이터 출력하기
+            dos.writeUTF("홍길동");
+            dos.writeInt(34);
+            dos.writeDouble(175.8);
+            dos.writeChar('남');
+            dos.writeBoolean(false);
+
+            // 파일에서 데이터 읽어오기
+            System.out.println(dis.readUTF());
+            System.out.println(dis.readInt());
+            System.out.println(dis.readDouble());
+            System.out.println(dis.readChar());
+            System.out.println(dis.readBoolean());
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
